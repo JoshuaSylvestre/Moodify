1f0c99b41c6c0d4af43e8e2bd2bc6ffd3781bc3d
Joshua Sylvestre <joshuasylvestre@knights.ucf.edu>
2019-08-03 14:28:50 -0400

    Adds spotify API, Google Vision AI API, Webcam Capture

--- backend/app.js
+++ backend/app.js
@@ -3,30 +3,20 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const querystring = require('querystring');
 const logger = require('morgan');
+var request = require('request');
 var cors = require('cors');
+
+var SpotifyWebApi = require('spotify-web-api-node');
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 app.use(cors());
-
-const port = 5000;
-
-// Used when generating any kind of tokens
-const twilioAccountSid = '';
-const twilioApiKey = '';
-const twilioApiSecret = '';
-
-// Endpoint to generate access token
-app.get('/', (request, response) => {
-
-  // Response being sent
-  response.send({
-    identity:  'test',
-    });
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,10 +26,144 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public'))).use(cookieParser());
+
+
+
+var scopes = ['user-library-read','user-library-modify'];
+var redirect_uri = process.env.REDIRECT_URI;
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
+var stateKey = 'spotify_auth_state';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: client_id,
+  redirectUri: redirect_uri,
+  clientSecret: client_secret
+});
+
+// var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+
+var generateRandomString = function (length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+// Endpoint to generate access token
+app.get('/', (req, res) => {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+  
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email user-library-read';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+
+  // Response being sent
+  // response.send({
+  //   identity:  'test',
+  //   });
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/callback', function (req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+  
+  console.log('state: ' + state);
+  console.log('\nstored state: ' + storedState);
+  console.log('cookie: ' + req.cookies);
+
+  if (state === null || state !== storedState) {
+    res.redirect('http://localhost:3000/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function (error, response, body) {
+          console.log(body);
+        });
+
+        // // we can also pass the token to the browser to make requests from there
+        res.redirect('http://localhost:3000/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('http://localhost:3000/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+async function quickstart() {
+
+  // Performs label detection on the image file
+  const [result] = await client.faceDetection('./happy2.jpg');
+  
+  const faces = result.faceAnnotations;
+  console.log('Faces:');
+  console.log(faces.length);
+
+  faces.forEach((face, i) => {
+    console.log(`  Face #${i + 1}:`);
+    console.log(`    Joy: ${face.joyLikelihood}`);
+    console.log(`    Anger: ${face.angerLikelihood}`);
+    console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+    console.log(`    Surprise: ${face.surpriseLikelihood}`);
+  });
+}
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,79 @@
 /* eslint-disable linebreak-style */
 import React, { Component } from 'react';
-import SpeechRecognition from 'react-speech-recognition';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
   constructor() {
+    
     super();
+    const params = this.getHashParams();
+    // console.log(params);
+    const token = params.access_token;
+
+    if(token)
+    {
+      spotifyApi.setAccessToken(token);
+    }
+
     this.state = { 
-      users: []
+      loggedIn: token ? true: false,
+      albums: []
     };
+
+  }
+
+  onTakePhoto(dataUri) {
+    console.log(dataUri);
+  }
+
+  getSavedAlbums() {
+    spotifyApi.getMySavedAlbums()
+    .then((response) => {
+      this.setState({
+        albums: response.items
+      });
+      console.log(this.state.albums);
+    })
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
   }
 
   componentDidMount() {
   }
 
   render() {
-    const { users } = this.state;
     return (
-      <div className="App">
-      <h1>Time to go sicko mode</h1>
+    <div className='App'>
+        <div>
+          <a href='http://localhost:8888'> Login to Spotify </a>
+        </div>
+        <div>
+        {
+        this.state.loggedIn &&
+        <button onClick={() => this.getSavedAlbums()}>
+          Get Saved Albums!
+          </button>
+        }
       </div>
+      <div>
+        <Camera 
+          onTakePhoto = {(dataUri) => {this.onTakePhoto(dataUri); }}
+        />
+      </div>
+    </div>
     );
   }
 }
